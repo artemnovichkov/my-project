d257ddfaa6ba6ca1ad4f11f22110a641191b1072
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-20 13:05:41 +0600

    Refactor script

--- MyFrameworkExample/universal-framework.sh
+++ MyFrameworkExample/universal-framework.sh
@@ -15,7 +15,7 @@ export XCODE_XCCONFIG_FILE="$xcconfig"
 # Make sure the output directory exists
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
 
-# Next, work out if we're in SIM or DEVICE
+# Next, work out if we're in SIMULATOR or DEVICE
 xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration ${CONFIGURATION} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration ${CONFIGURATION} -sdk iphoneos ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
@@ -28,6 +28,5 @@ cp -R "${SYMROOT}/Debug-iphonesimulator/${FRAMEWORK_NAME}.framework/Modules/${FR
 # Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
 lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "${SYMROOT}/Debug-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
 
-export
 # Step 5. Convenience step to copy the framework to the main project's directory
-cp -a "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework" "${PROJECT_ROOT_FOLDER}/MyProject/MyProject/Frameworks/${FRAMEWORK_NAME}.framework"
+cp -a "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework" "${PROJECT_ROOT_FOLDER}/MyProject/MyProject/Frameworks"
