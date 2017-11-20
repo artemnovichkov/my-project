b61bb74ed50d7c6da9c4e3c74e4b1fefb841c639
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-20 11:28:43 +0600

    Update README and project structure

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-## My Project
+## 📦 Frameworks
 
-A test project for showing of first-party framework integration. The main goal of this approach is to link a compiled static frameworks to prevent extra rebuilding.
+A test project for showing first-party framework integration. The main goal of this approach is to link a compiled static frameworks to prevent extra rebuilding.
 
 Current structure:
 - `MyWorkspace` - a workspace with all projects and playgrounds
-- `MyProject` - a main app with linked static framework.
+- `MyProject` - a main app with linked static frameworks
 - `MyFrameworkExample` - an example with a `MyFramework` target for isolated framework testing
-- `MyFrameworkPlayground` - a playground for framework testing
+- `MyFrameworkPlayground` - an playground with a `MyFramework` framework for isolated framework testing
+
+## 🚀 Main flow
+
+1. Add new features to `MyFramework`.
+2. Test it:
+ - via `MyFrameworkExample`. It let you store different enviroments.
+ - via `MyFrameworkPlayground`. The same as above plus no need to run a simulators.
+3. Run `MyFramework-Universal` target. It creates an universal static framework and save it to `Frameworks` folder in the main project.
+
+## 🤔 Current issues
+
+- Manual configuration of new projects for this scheme
+- Playgrounds don't work with static frameworks. So after `MyFramework-Universal` target running you should clean the Build folder.
+- You must run `MyFramework-Universal` target manually after every change in the framework.
+- Workspace has one DerivedData folder for all projects. After main project cleaning all frameworks data will be deleted.
---
+++ universal-framework.sh
@@ -0,0 +1,31 @@
+UNIVERSAL_OUTPUTFOLDER=${BUILD_DIR}/${CONFIGURATION}-universal
+FRAMEWORK_NAME="MyFramework"
+
+# Step 1. Build static frameworks
+
+# Make env variable with static linking
+xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
+trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
+
+echo "MACH_O_TYPE = staticlib" >> $xcconfig
+
+export XCODE_XCCONFIG_FILE="$xcconfig"
+
+# Make sure the output directory exists
+mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
+
+# Next, work out if we're in SIM or DEVICE
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration ${CONFIGURATION} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${FRAMEWORK_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+
+# Step 2. Copy the framework structure (from iphoneos build) to the universal folder
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${FRAMEWORK_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+
+# Step 3. Copy Swift modules from iphonesimulator build (if it exists) to the copied framework directory
+cp -R "${SYMROOT}/Debug-iphonesimulator/${FRAMEWORK_NAME}.framework/Modules/${FRAMEWORK_NAME}.swiftmodule/." "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework/Modules/${FRAMEWORK_NAME}.swiftmodule"
+
+# Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
+lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "${SYMROOT}/Debug-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
+
+# Step 5. Convenience step to copy the framework to the main project's directory
+cp -a "${UNIVERSAL_OUTPUTFOLDER}/${FRAMEWORK_NAME}.framework" "$PWD/MyProject/Frameworks/${FRAMEWORK_NAME}.framework"
